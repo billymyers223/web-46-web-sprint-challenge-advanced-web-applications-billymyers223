4337c8bcd04e1ec53d1063e7238054a736539c91
billmyers223 <billymyers223@gmail.com>
2021-09-17 14:15:01 -0500

    Finished private route, axioswithauth, & logout button functionality

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Switch  } from "react-router-dom";
-
+import PrivateRoute from "./components/PrivateRoute";
+import BubblePage from './components/BubblePage';
 import Login from "./components/Login";
 import "./styles.scss";
-
+import axiosWithAuth from "./helpers/axiosWithAuth";
 function App() {
+  const logOut = () =>{
+    axiosWithAuth().post('api/logout')
+      .then(res =>{
+        console.log(res)
+        localStorage.removeItem('token')
+        window.location.href='login';
+      }).catch(err => console.error(err))
+  }
   return (
     <Router>
       <div className="App">
         <header>
           Color Picker Sprint Challenge
-          <a data-testid="logoutButton" href="#">logout</a>
+          <a data-testid="logoutButton" onClick={logOut}>logout</a>
         </header>
       </div>
       <Switch>
+      <PrivateRoute exact path="/bubbles" component={BubblePage} />
         <Route path = '/login' component ={Login}/>
         <Route exact path="/" component={Login} />
       </Switch>
